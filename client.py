9af89be74053c92b3563c939705581d10c567927
Pip Doun <pipdoun@gmail.com>
2021-09-01 18:40:23 +0300

    * Project rebased - all bugs fixed - more controllable serverside

--- client.py
+++ client.py
@@ -1,55 +1,74 @@
 import socket
-import tools.helper as helper
 import threading
+import tools.helper as helper
 
-HEADER = 128
-PORT = 5050
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!q"
-SERVER = socket.gethostbyname(socket.gethostname())
-ADDR = (SERVER, PORT)
+hp = helper.Helper(helper._FORMAT)
 
-hp = helper.Helper(FORMAT)
+class Client:
+    def __init__(self,SERVER:str,PORT:int):
+        self._CSOCK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._ADDR = (SERVER,PORT)
+        self._PORT = PORT
+        self._SERVER = SERVER
+        self._RUN = False
+        self._Threads = []
+        self._shutReq = False
 
+    def __loadThreads(self):
+        recvTh = threading.Thread(target=self.__recvResponse,name="recvTh",daemon=True)
+        sendTh = threading.Thread(target=self.__sendResponse,name="sendTh",daemon=False)
 
-def send(msg:str,sock:socket.socket):
-    sock.send(hp.utf8len(msg))
-    sock.send(msg.encode(FORMAT))
+        self._Threads.append(recvTh)
+        self._Threads.append(sendTh)
 
-def getResponse(sock:socket.socket):
-    while True:
-        try:
-            msg_len = int(sock.recv(HEADER).decode(FORMAT))
-            if msg_len:
-                msg = sock.recv(msg_len).decode(FORMAT)
-                if msg == "!q":
-                    print("\n[CLOSING]  Server is down")
-                    break
-                else:
-                    print("Server: "+msg)
-        except socket.error as err:
-            print(f"[ERROR]  {err}")
-            break
+    def __runThreads(self):
+        for t in self._Threads:
+            t.start()
+            
+    def __send(self,cmd:str):
+        self._CSOCK.send(hp.utf8len(cmd))
+        self._CSOCK.send(cmd.encode(helper._FORMAT))
 
-def connect():
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(ADDR)
-    connexHandler = threading.Thread(target=getResponse, args=(client,),daemon=True)
-    connexHandler.start()
-    while True:
-        try:
-            msg = str(input("> "))
-            send(msg,client)
-            if msg == DISCONNECT_MESSAGE:
-                connexHandler.join()
+    def __recvResponse(self):
+        while self._RUN:
+            try:
+                msg_len = self._CSOCK.recv(helper._HEADER).decode(helper._FORMAT)
+                if msg_len:
+                    msg = self._CSOCK.recv(int(msg_len)).decode(helper._FORMAT)
+                    if msg == f"{helper._DISC}":
+                        print("\n[CLOSING]  Server is down")
+                        self.__send("!q")
+                        self._shutReq = True
+                    else:
+                        print(f"Server: {msg}")
+            except socket.error as err:
+                print(f"[ERROR-1]  {err}")
+                self._shutReq = True
+                #break 
+
+    def __sendResponse(self):
+        while self._RUN:
+            try:
+                msg = str(input("> "))
+                if not self._shutReq:
+                    self.__send(msg)
+                    if msg == helper._DISC:
+                        print("[CLOSE]  DISCONNECTED")
+                        self._shutReq = True
+            except socket.error as err:
+                print(f"[ERROR-2]  {err}")
+                self._shutReq = True
                 break
-        except socket.error as err:
-            print(f"[ERROR]  {err}")
-            connexHandler.join()
-            break
+
+    def connect(self):
+        self._RUN = True
+        self._CSOCK.connect(self._ADDR)
+        print(f"[CONNEXD] Server is listening on {self._SERVER}")
+        self.__loadThreads()
+        print("[READY]  Client is ready for processes ! ")
+        self.__runThreads()
 
 if __name__ == "__main__":
-    connect()
-    input("> done")
-
-
+    cli = Client(socket.gethostbyname(socket.gethostname()),5050)
+    cli.connect()
+        
--- server.py
+++ server.py
@@ -1,131 +1,138 @@
 import socket
-import threading
 import tools.helper as helper
+import threading
+import time
 
-HEADER = 64
-PORT = 5050
-SERVER = socket.gethostbyname(socket.gethostname())
-ADDR = (SERVER, PORT)
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!q"
-ACTIVES = {}
-RUN = False
+hp = helper.Helper(helper._FORMAT)
 
-hp = helper.Helper(FORMAT)
+class Server:
+  
+    def __init__(self,SERVER:str,PORT:int):
+        
+        self._PORT = PORT
+        self._SERVER = SERVER
+        self._ADDR = (SERVER,PORT)
+        self._SSOCK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._RUN = False
+        self._ACTIVES = {}
+        self._Threads = []
+        self._shutReq = False
 
+    def __send(self,cmd:str,ct:int):
+        print("[ ! ]  Send Response triggered !")
+        conn =1
+        for sock in self._ACTIVES:
+            if ct == conn:
+                print(f"[SOCKET_NAME]  {conn}")
+                sock.send(hp.utf8len(cmd))
+                sock.send(cmd.encode(helper._FORMAT))
+                print("[SENT]  Message has sent ")
+                break
+            conn+=1
 
-def sendCmd(cmd, cli):
-    cli.send(hp.utf8len(cmd))
-    cli.send(cmd.encode(FORMAT))
-
-def sendAll(cmd,all:list):
-    for addr in all:
-        sendCmd(cmd,addr)
-
-def recv(conn, addr):
-    print(f"\n[NEW CONNECTION] {addr[1]} connected.")
-    while True:
-        print("Client bridge")
-        try:
-            msg_len = conn.recv(HEADER).decode(FORMAT)
-            if msg_len:
-                msg = conn.recv(int((msg_len))).decode(FORMAT)
-                if msg == DISCONNECT_MESSAGE:
-                    print(f"\n[{addr[1]}] DISCONNECTED")
-                    break
-                else:
-                    print(f"\n[{addr[1]}] {msg}")
-        except socket.error as err:
-            print(f"[ERROR]  {err}")
-            break
-    del ACTIVES[conn]
-    conn.close()
-
-def serverManager(isrun:bool):
-    while True:
-        print("ServerMan Running")
-        try:
-            cmd = input("cmd: > ")
-            if cmd:
-                if cmd == DISCONNECT_MESSAGE:
-                    print("[CLOSING]  server will be closed ...")
-                    # send message about server is down
-                    sendAll(cmd,ACTIVES)
-                    isrun = False
-                    break
-                elif cmd.split()[0] == "!c":
-                    listClients()
-                elif cmd.split()[0] == "sel":
-                    listClients()
-                    clct = 1
-                    cmd = input(": > ")
-                    for client in ACTIVES:
-                        if int(cmd) == clct:
-                            cmdToCli = str(input("send: "))
-                            sendCmd(cmdToCli, client)
-                        clct += 1
-        except socket.error as err:
-            print(f"[ERROR]  {err}")
-            break
-
-def listClients():
-    ct = 1
-    if len(ACTIVES) == 0:
-        print("No Online Clients")
-    else:
+    def __sendAll(self,cmd:str):
+        for online in self._ACTIVES:
+            online.send(hp.utf8len(cmd)),
+            online.send(cmd.encode(helper._FORMAT))
+            
+    def __listClients(self):
+        ct = 1
         print("-- ACTIVE CLIENTS --\n")
-        for cl in ACTIVES:
-            print(f"[{ct}]  {ACTIVES[cl][1]}")
+        for cl in self._ACTIVES:
+            print(f"[{ct}]  {self._ACTIVES[cl][1]}")
             ct += 1
         print("\n-------------------- ")
+        return len(self._ACTIVES)
 
-def connexManager(sock:socket.socket,onlines:list,isrun:bool):
-    while isrun:
-        print("ConnexMan running...")
-        conn, addr = sock.accept()
-        onlines[conn] = addr
-        recv(conn,addr,isrun)
+    def __close_server(self):
+        while True:
+            if self._shutReq:
+                print("[ ! ]  All clients warned. ")
+                self.__sendAll(helper._DISC)
+                time.sleep(2)
+                for cli in self._ACTIVES:
+                    cli.close()
+                self._ACTIVES.clear()
+                if not self._ACTIVES:
+                    print("\n[ALL KILLED]")
+                    self._shutReq = False
+                #break
 
+    def __connexMan(self):
+        while self._RUN:
+            try:
+                conn, addr = self._SSOCK.accept()
+                self._ACTIVES[conn] = addr
+                print(f"\n[NEW CONNECTION] {addr[1]} connected.")
+            except socket.error as err:
+                print(f"[ERROR-1]  {err}")
+                print("[CLOSING]")
 
-def run(isrun:bool,onlines:list):
-
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(ADDR)
-
-    print("[STARTING] server is starting...")
-
-    server.listen()
-    print(f"[LISTENING] Server is listening on {SERVER}")
-    isrun = True
-    serverHandler = threading.Thread(target=serverManager,args=(isrun,), daemon=False)
-    serverHandler.start()
-    
-    print("[HANDLING] server is able to commands")
-
-    # connexHandler = threading.Thread(target=connexManager, args=(server,onlines,isrun),daemon=False)
-    # connexHandler.start()
-
-    while True:
-        try:
-            if not serverHandler.is_alive():
-                print(f"\nhandler status: {serverHandler.is_alive()}")
-                serverHandler.join()
-                break
-            else:
-                print("\n[WAITING]  Server is waiting")
-                
-            conn, addr = server.accept()
-            ACTIVES[conn] = addr
-
-            clientHandler = threading.Thread(target=recv, args=(conn, addr), daemon=True)
+            clientHandler = threading.Thread(target=self.__clientBridge__,args=(conn,addr),daemon=True)
             clientHandler.start()
 
-            print(f"[ACTIVE CONNECTIONS]  {len(ACTIVES)}")
-        except socket.error as msg:
-            print("Error: " + msg)
-            print("[ERROR]  server will be closed ...")
-            break
+    def __clientBridge__(self,conn:socket.socket,addr):
+        
+        while not self._shutReq:
+            try:
+                msg_len = conn.recv(helper._HEADER).decode(helper._FORMAT)
+                if msg_len:
+                    msg = conn.recv(int((msg_len))).decode(helper._FORMAT)
+                    if msg == helper._DISC:
+                        print(f"\n[{addr[1]}] DISCONNECTED")
+                        conn.close()
+                        del self._ACTIVES[conn]
+                        break
+                    else:
+                        print(f"\n[{addr[1]}] {msg}")
+            except socket.error as err:
+                print(f"[ERROR-2]  {err}")
+                break
+        
+        
+
+    def __sendResponse(self):
+        while self._RUN:
+            try:
+                cmd = input("cmd: > ")
+                if cmd:
+                    if cmd == helper._DISC:
+                        print("[CLOSING]  server will be closed ...")
+                        self._shutReq = True
+                        time.sleep(2)
+                    elif cmd.split()[0] == "!c":
+                        self.__listClients()
+                    elif cmd.split()[0] == "!a":
+                        cmd = input("send: ")
+                        self.__sendAll(cmd)
+                    elif cmd.split()[0] == "sel":
+                        if self.__listClients():
+                            ct = int(input(": > "))
+                            cmd = input("send: ")
+                            self.__send(cmd,ct)
+                        else:
+                            print("\n[ ! ]  No Online Client Exist")
+            except socket.error as err:
+                print(f"[ERROR-3]  {err}")
+                self._shutReq = True
+                break
+
+    def run(self):
+        self._SSOCK.bind(self._ADDR)
+        print("[STARTING] server is starting...")
+        self._SSOCK.listen()
+        print(f"[LISTENING] Server is listening on {self._SERVER}")
+        self._RUN = True
+        sendTh = threading.Thread(target=self.__sendResponse,daemon=False)
+        sendTh.start()
+        print("[READY] server is able to commands")
+        connexTh = threading.Thread(target=self.__connexMan,daemon=True)
+        connexTh.start()
+        
+        threading.Thread(target=self.__close_server,daemon=False).start()
 
 if __name__ == "__main__":
-    run(RUN,ACTIVES)
-    input("> done")
+
+    s = Server(socket.gethostbyname(socket.gethostname()),5050)
+    s.run()
+    #input("Done")
